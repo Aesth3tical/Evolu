d34b3d40b17370ea7af08758d0bf6da8b7901e8a
Aesth3tical <danielrperkins05@gmail.com>
2021-12-30 00:52:22 +0000

    Minor stuff

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { TOKEN, CLIENTID, GUILDID } = process.env;
+const { TOKEN, CLIENTID, GUILDID, OWNER, ADMIN_PREFIX } = process.env;
 
 module.exports = {
     name: 'messageCreate',
     trigger: 2,
     async execute (message, client) {
-        if (message.content === '%loadslash') {
-            if (process.env.OWNER !== message.author.id && message.author.id !== client.user.id) {
+        if (message.content === `${ADMIN_PREFIX}loadslash`) {
+            if (OWNER !== message.author.id && message.author.id !== client.user.id) {
                 const notAllowed = new Discord.MessageEmbed()
                     .setColor('RED')
                     .setDescription('Only the bot owner is allowed to run text commands, as they are only used to load and reload the slash commands. Try those instead :smile:!')
